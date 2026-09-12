149242c8a130cc5429802989722669dd2f29b145
asadulsameerbd <asadulsameerbd>
2026-09-12 13:44:28 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 5173, proxy: { "/api": "http://localhost:5000" } },
+  server: {
+    port: 5173,
+    proxy: { "/api": "https://takeiteasyserver.vercel.app/" },
+  },
 });
